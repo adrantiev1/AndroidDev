17d62d8170481a0fa0a113d95bc2fa1973baaca2
adrantiev1 <35266709+adrantiev1@users.noreply.github.com>
2019-01-15 13:26:10 -0700

    init a messaging app

---
+++ Chatter/app/src/main/java/com/example/adrantiev1/chatter/ChatterReciveActivity.java
@@ -0,0 +1,13 @@
+package com.example.adrantiev1.chatter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatterReciveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatter_recive);
+    }
+}
