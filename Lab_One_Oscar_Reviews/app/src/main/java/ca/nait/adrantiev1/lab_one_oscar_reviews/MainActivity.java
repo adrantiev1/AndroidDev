ef278b0e0eac485161170e5252ed539e0e3ccd93
adrantiev1 <35266709+adrantiev1@users.noreply.github.com>
2019-01-29 14:50:22 -0700

    initialized lab 1
    created all the displays, no functionality

---
+++ Lab_One_Oscar_Reviews/app/src/main/java/ca/nait/adrantiev1/lab_one_oscar_reviews/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.nait.adrantiev1.lab_one_oscar_reviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
