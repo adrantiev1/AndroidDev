2885bb2b7c92bdbfd7eb86d29c5fa78769bef326
adrantiev1 <35266709+adrantiev1@users.noreply.github.com>
2019-02-06 08:29:09 -0700

    worked on display reviews

--- Lab_One_Oscar_Reviews/app/src/main/java/ca/nait/adrantiev1/lab_one_oscar_reviews/displayReviewActivity.java
+++ Lab_One_Oscar_Reviews/app/src/main/java/ca/nait/adrantiev1/lab_one_oscar_reviews/displayReviewActivity.java
@@ -7,10 +7,13 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,59 +25,78 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class displayReviewActivity extends AppCompatActivity {
     RadioGroup rg;
 
-    SharedPreferences prefs;
-    View mainview;
+
+
+    ArrayList<HashMap<String,String>> reviews = new ArrayList<HashMap<String, String>>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_review);
 
-        rg = (RadioGroup)findViewById(R.id.rg_category);
-        String value = ((RadioButton)findViewById(rg.getCheckedRadioButtonId()))
-                .getText().toString();
+        /*rg = (RadioGroup)findViewById(R.id.rg_category);
+        String category = ((RadioButton)findViewById(rg.getCheckedRadioButtonId()))
+                .getText().toString();*/
+
+        String category = "";
+        displayReviews(category);
 
 
-        displayReviews(value);
-
-        prefs = PreferenceManager.getDefaultSharedPreferences(this);
-
-        mainview = findViewById(R.id.layout_main_activity);
-        String bgColor = prefs.getString("main_bg_color", "#ccdd3c");
-        mainview.setBackgroundColor(Color.parseColor(bgColor));
     }
 
-    private void displayReviews(String value)
+    private void displayReviews(String category)
     {
-        String[] keys = new String[]{"user","date","category","nominee","review"};
+        String[] keys = new String[]{"DATE","REVIEWER","CATEGORY","NOMINEE","REVIEW"};
+        int[] ids = new int[]{R.id.custom_row_date,R.id.custom_row_user,R.id.custom_row_category,R.id.custom_row_nominee,R.id.custom_row_review};
+        SimpleAdapter adapter = new SimpleAdapter(this,reviews,R.layout.custom_row_layout,keys,ids);
+        ListView listView= (ListView)findViewById(R.id.listview_receive_review);
+        populateList(category);
+        listView.setAdapter(adapter);
 
 
 
+    }
 
-
-
-        /*BufferedReader in = null;
-        TextView textView = (TextView) findViewById(R.id.textbox_receive_review);
+    private void populateList(String category)
+    {
+        category = "film";
+        BufferedReader in = null;
         try
         {
             HttpClient client = new DefaultHttpClient();
             HttpGet request = new HttpGet();
-            request.setURI(new URI(" http://www.youcode.ca/Lab01Servlet?CATEGORY=" + value));
+            request.setURI(new URI("http://www.youcode.ca/Lab01Servlet?CATEGORY="+ category));
             HttpResponse response = client.execute(request);
             in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-            String line = "";
-            while((line = in.readLine())!= null)
+            String field = "";
+            while((field = in.readLine())!= null)
             {
-                textView.append(line + "\n");
+//                Log.d(TAG, "reading from server - vslue =" + Value );
+                HashMap<String,String> temp = new HashMap<String, String>();
+                temp.put("DATE",field);
+                field = in.readLine();
+                temp.put("REVIEWER",field);
+                field = in.readLine();
+                temp.put("CATEGORY",field);
+                field = in.readLine();
+                temp.put("NOMINEE",field);
+                field = in.readLine();
+                temp.put("REVIEW",field);
+                reviews.add(temp);
+
             }
+            in.close();
         }
         catch (Exception e)
         {
-            Toast.makeText(this,"Error: " + e,Toast.LENGTH_LONG).show();
-        }*/
+            Toast.makeText(this,"Error: "+ e, Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -100,8 +122,12 @@ public class displayReviewActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+
+        MenuInflater inflator = this.getMenuInflater();
+        inflator.inflate(R.menu.main_menu,menu);
+        return true;
     }
 
 
